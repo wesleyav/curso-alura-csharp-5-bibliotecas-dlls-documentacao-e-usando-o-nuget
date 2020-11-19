9afa7fc2361ef6dce9099ea74d0df2bac1dbc3a5
Wesley <wesley.contato@hotmail.com>
2020-11-19 19:11:44 -0300

    Referencias de projetos

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ByteBank.Modelos;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -10,6 +11,11 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
         }
     }
 }
